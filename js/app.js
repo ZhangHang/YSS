1092c4c965135e8be1d19c47faae4589abb51dbf
ZhangHang <zh199111@gmail.com>
2015-11-05 03:53:10 +0800

    Refactor

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var TASK_NAME_UNLOCK = "TASK_NAME_UNLOCK"
 var TASK_NAME_NAVIGATE_TO_NEXT_PAGE = "TASK_NAME_NAVIGATE_TO_NEXT_PAGE"
 
 var Animator = (function() {
+  var DEFAULT_ANIMATION_DURATION = 1000
+
   var core = {}
 
   /*
@@ -11,6 +13,8 @@ var Animator = (function() {
     var store = {}
     var timeoutIds = []
     store.addAction = function(action, delay) {
+      console.assert(action != undefined)
+      console.assert(delay != undefined)
       timeoutIds.push(setTimeout(action, delay))
     }
     store.removeAllActions = function() {
@@ -26,21 +30,57 @@ var Animator = (function() {
    * delay   : millisecond
    * options :
    *   infinite: Bool
-   *
+   *   callback: Function
    */
-  core.animate = function(animationName, node, delay, callback, options) {
-    var isInfinite = (options || {}).hasOwnProperty('infinite') ? options.infinite : false
+  core.animate = function(animationName, node, delay, options) {
+    var _delay = delay || 0
+    var _options = (function() {
+      var _options = {
+        infinite: false,
+        callback: function() {}
+      }
+      for (var attrname in options || {}) {
+        _options[attrname] = options[attrname];
+      }
+      return _options
+    })()
 
-    TimeoutActionStore.addAction(function() {
-      $(node).addClass("animated" + (isInfinite ? ".infinite" : "") + " " + animationName);
-      TimeoutActionStore.addAction(callback || function() {}, 1000)
-    }, delay)
+    var core = {}
+    var afterTimeOffset = 0;
+    var afterActions = []
+    core.after = function(__action, __delay) {
+      afterActions.push({
+        delay: afterTimeOffset + __delay,
+        action: __action
+      })
+      afterTimeOffset += __delay;
+      return core;
+    }
+    core.done = function() {
+      TimeoutActionStore.addAction(function() {
+        $(node).addClass("animated" + (_options.infinite ? " infinite" : "") + " " + animationName);
+        TimeoutActionStore.addAction(_options.callback, DEFAULT_ANIMATION_DURATION)
+        afterActions.forEach(function(actionPack) {
+          TimeoutActionStore.addAction(actionPack.action, actionPack.delay)
+        })
+      }, _delay)
+    }
+    return core;
+  }
+
+  animateActionFactory = function(animationName) {
+    return function() {
+      var node = arguments[0]
+      var delay = arguments[1]
+      var options = arguments[2]
+      console.assert(node != undefined)
+      return core.animate(animationName, node, delay, options)
+    }
   }
 
   // MARK: - Custom
-  core.fadeIn = function(node, delay, callback, options) {
-    core.animate("fadeIn", node, delay, callback, options)
-  }
+  core.fadeIn = animateActionFactory("fadeIn")
+  core.flash = animateActionFactory("flash")
 
   // MARK: -
   core.registerCustomAction = function(action, delay, callback) {
@@ -135,50 +175,85 @@ var Inbox = (function() {
 
   pages.push({
     render: function() {
-      Animator.fadeIn(this.find("#logo"), 0)
-      Animator.fadeIn(this.find("#seperator_top"), 600)
-      Animator.fadeIn(this.find("#seperator_bottom"), 1200)
-      Animator.fadeIn(this.find("#text_left"), 1600)
-      Animator.fadeIn(this.find("#text_righ"), 1900)
-      Animator.fadeIn(this.find("#text_bottom"), 2100)
-      Animator.fadeIn(this.find("#drop"), 2200)
-    }
-  })
-
-  pages.push({
-    render: function() {
       var self = this
-      Animator.fadeIn(this.find("#drop"), 0)
-      Animator.fadeIn(this.find("#inner_circle"), 600)
-      Animator.fadeIn(this.find("#outter_circle"), 1200)
-      Animator.fadeIn(this.find("#text"), 1600)
-      Animator.fadeIn(this.find("#hand"), 1900, function() {
-        self.find(".tapArea").on('click', function() {
-          Animator.animate("fadeOutDown", self.find("#drop"));
-          Animator.registerCustomAction(function() {
-            Inbox.post(TASK_NAME_NAVIGATE_TO_NEXT_PAGE)
-          }, 900)
-        })
-      })
+      Animator.fadeIn(self.find("#logo"))
+        .after(function() {
+          Animator.fadeIn(self.find("#seperator_top")).done()
+        }, 600)
+        .after(function() {
+          Animator.fadeIn(self.find("#seperator_bottom")).done()
+        }, 500)
+        .after(function() {
+          Animator.fadeIn(self.find("#text_left")).done()
+        }, 400)
+        .after(function() {
+          Animator.fadeIn(self.find("#text_righ")).done()
+        }, 300)
+        .after(function() {
+          Animator.fadeIn(self.find("#text_bottom")).done()
+        }, 200)
+        .after(function() {
+          Animator.fadeIn(self.find("#drop")).done()
+        }, 100)
+        .done()
     }
   })
 
   pages.push({
     render: function() {
 
-      Animator.fadeIn(this.find("#info_container"), 0)
-      Animator.fadeIn(this.find("#info_text"), 100)
+      var self = this
 
-      Animator.fadeIn(this.find("#step_1"), 600)
-      Animator.fadeIn(this.find("#step_1_text"), 700)
+      Animator.fadeIn(self.find("#drop"))
+        .after(function() {
+          Animator.fadeIn(self.find("#text")).done()
+        }, 600)
+        .after(function() {
+          Animator.fadeIn(self.find("#hand")).done()
+        }, 600)
+        .after(function() {
+          Animator.fadeIn(self.find("#inner_circle"), 0, {
+            infinite: true
+          }).done()
+          Animator.fadeIn(self.find("#outter_circle"), 500, {
+            infinite: true
+          }).done()
+          self.find(".tapArea").on('click', function() {
+            Animator.animate("fadeOutDown", self.find("#drop")).done()
+            Animator.registerCustomAction(function() {
+              Inbox.post(TASK_NAME_NAVIGATE_TO_NEXT_PAGE)
+            }, 900)
+          })
+        },600)
+        .done()
+    }
+  })
 
-      Animator.fadeIn(this.find("#step_2"), 1700)
-      Animator.fadeIn(this.find("#step_2_text"), 1800)
+  pages.push({
+    render: function() {
+      Animator.fadeIn(this.find("#info_container")).done()
+      Animator.fadeIn(this.find("#info_text"), 100).done()
 
-      Animator.fadeIn(this.find("#step_3"), 2800)
-      Animator.fadeIn(this.find("#step_3_text"), 2900)
+      Animator.fadeIn(this.find("#step_1"), 600).done()
+      Animator.fadeIn(this.find("#step_1_text"), 700).done()
 
-      Animator.fadeIn(this.find("#arrow"), 3600)
+      Animator.fadeIn(this.find("#step_2"), 1700).done()
+      Animator.fadeIn(this.find("#step_2_text"), 1800).done()
+
+      Animator.fadeIn(this.find("#step_3"), 2800).done()
+      Animator.fadeIn(this.find("#step_3_text"), 2900).done()
+
+      Animator.fadeIn(this.find("#arrow"), 3600).done()
+    }
+  })
+
+  pages.push({
+    render: function() {
+      Animator.fadeIn(this.find("#main")).done()
+      Animator.fadeIn(this.find("#dot"), 300).done()
+      Animator.fadeIn(this.find("#drop"), 600).done()
+      Animator.fadeIn(this.find("#skin_bag"), 500).done()
+      Animator.fadeIn(this.find("#skin_bag_inner"), 400).done()
     }
   })
 
@@ -212,10 +287,5 @@ var Inbox = (function() {
     }
   })
 
-  pages.push({
-    render: function() {
-
-    }
-  })
   return pages;
 })())
