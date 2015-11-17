70ff1615dcd5feea0b0e8455b97a26c8d1271560
ZhangHang <zh199111@gmail.com>
2015-11-17 18:59:31 +0800

    Update hand in change page

--- app/pages/change.js
+++ app/pages/change.js
@@ -1,6 +1,5 @@
 pageStack.set('change', {
   render: function(self, incrementer) {
-    Animator.fadeIn(self.find("#sun"), incrementer.next()).done()
     Animator.fadeIn(self.find("#head"), incrementer.next()).done()
     Animator.fadeIn(self.find("#hand"), incrementer.next()).done(function() {
       Animator.removeFadeIn(this)
@@ -8,20 +7,15 @@ pageStack.set('change', {
         infinite: true
       }).done()
     })
-    Animator.fadeIn(self.find("#arrow"), incrementer.next()).done()
 
-    self.find(".tapArea").on("click", function() {
+    self.on("click", function() {
       $(this).off()
       incrementer.reset()
-      Animator.fadeOut(self.find("#sun")).done()
       Animator.fadeOut(self.find("#hand")).done(function() {
         this.remove()
       })
       Animator.fadeOut(self.find("#head")).done()
-
-      Animator.fadeIn(self.find("#scene-1-sun"), incrementer.next()).done()
       Animator.fadeIn(self.find("#scene-1-head"), incrementer.next()).done()
-
       Animator.fadeIn(self.find(".next-page-arrow"), incrementer.next()).done(function() {
         Inbox.post(TASK_NAME_UNLOCK_PAGE)
       })
