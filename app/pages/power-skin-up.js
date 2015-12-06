4cf5f68edba241acb9806f16dee229b45f9f6c56
ZhangHang <zh199111@gmail.com>
2015-12-07 03:08:42 +0800

    Update assets

--- app/pages/power-skin-up.js
+++ app/pages/power-skin-up.js
@@ -12,24 +12,23 @@ pageStack.set('power-skin-up', {
     Animator.fadeIn(container.find(".drop"), incrementer.last()).done(function() {
       Animator.fadeOut(container.find(".drop")).done()
       Animator.fadeIn(container.find(".drop-down")).done()
-      Animator.performAction(completionHandler, 400)
+      Animator.fadeIn(container.find(".drop-down-bottom"),400).done()
+      Animator.performAction(completionHandler, 1600)
     })
   },
   dismissScene1: function(self) {
     var container = self.find(".scene-1")
-    Animator.fadeOut(container.find(".cell")).done()
-    Animator.fadeOut(container.find(".drop-down")).done()
+    Animator.fadeOut(container).done()
   },
   enterScene2: function(self, incrementer, completionHandler) {
     incrementer.reset()
     var container = self.find(".scene-2")
     Animator.fadeIn(container.find(".cell"), incrementer.next()).done()
-    Animator.fadeIn(container.find(".mitochondria"), incrementer.last()).done()
-    Animator.fadeIn(container.find(".drop"), incrementer.last()).done()
+    Animator.fadeIn(container.find(".mitochondria"), incrementer.next(300)).done()
     Animator.performAction(function() {
       container.find(".mitochondria").removeClass("fadeIn")
       Animator.bounceIn(container.find(".mitochondria")).done()
-    }, incrementer.next(2000))
+    }, incrementer.next(1000))
     Animator.fadeIn(container.find(".light"), incrementer.next()).done()
     Animator.bounceIn(container.find(".text"), incrementer.next()).done(completionHandler)
   }
